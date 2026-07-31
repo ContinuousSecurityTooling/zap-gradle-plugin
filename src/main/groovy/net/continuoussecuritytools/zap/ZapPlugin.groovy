8821f9c794581ec84d95ac7b39e65d6324049666
Martin Reinhardt <martin@m13t.de>
2026-07-31 12:21:10 +0200

    fix: Correcting gradle build

---
+++ src/main/groovy/net/continuoussecuritytools/zap/ZapPlugin.groovy
@@ -0,0 +1,11 @@
+package net.continuoussecuritytools.zap
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class ZapPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+    }
+}
